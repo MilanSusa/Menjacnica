390419fd755442e6e5cddbc61960f5629a70c629
MilanSusa <milan_susa@hotmail.com>
2017-03-22 20:28:17 +0100

    Uvedena klasa TestMenjacnice koja implementira interfejs ValutaInterfejs u paketu test bez implementacija metoda

---
+++ Menjacnica/src/test/TestMenjacnice.java
@@ -0,0 +1,28 @@
+package test;
+
+import java.util.GregorianCalendar;
+
+import interfejs.ValutaInterfejs;
+import valuta.Kurs;
+
+public class TestMenjacnice implements ValutaInterfejs {
+
+	@Override
+	public void dodajKursValuteNaOdredjeniDan(String nazivValute, Kurs kurs, GregorianCalendar naDan) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void obrisiKursValuteNaOdredjeniDan(String nazivValute, Kurs kurs, GregorianCalendar naDan) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Kurs vratiKursValuteNaOdredjeniDan(String nazivValute, GregorianCalendar naDan) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
