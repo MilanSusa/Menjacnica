7166530dd705ea15a5cbd7050791ced3f0048ba4
MilanSusa <milan_susa@hotmail.com>
2017-03-22 19:04:06 +0100

    Uvedene redefinisane metode toString, hashCode i equals

--- Menjacnica/src/valuta/Valuta.java
+++ Menjacnica/src/valuta/Valuta.java
@@ -30,4 +30,31 @@ public class Valuta {
 		this.kurs = kurs;
 	}
 
+	@Override
+	public String toString() {
+		return "Valuta [naziv=" + naziv + ", skraceniNaziv=" + skraceniNaziv + ", kurs=" + kurs + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((kurs == null) ? 0 : kurs.hashCode());
+		result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+		result = prime * result + ((skraceniNaziv == null) ? 0 : skraceniNaziv.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof Valuta) {
+			Valuta v = (Valuta) obj;
+			if (v.getNaziv().equals(naziv) && v.getSkraceniNaziv().equals(skraceniNaziv) && v.getKurs().equals(kurs)) {
+				return true;
+			}
+			return false;
+		}
+		throw new RuntimeException("Objekat nije tipa Valuta.");
+	}
+
 }
