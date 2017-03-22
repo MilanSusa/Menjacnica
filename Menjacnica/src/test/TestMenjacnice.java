2d73a699134240bd2f05e384c8fc181feb2af2e5
MilanSusa <milan_susa@hotmail.com>
2017-03-22 20:56:22 +0100

    Implementirane sve metode u okviru klase TestMenjacnice

--- Menjacnica/src/test/TestMenjacnice.java
+++ Menjacnica/src/test/TestMenjacnice.java
@@ -1,28 +1,62 @@
 package test;
 
 import java.util.GregorianCalendar;
-
+import java.util.LinkedList;
 import interfejs.ValutaInterfejs;
 import valuta.Kurs;
+import valuta.Valuta;
 
 public class TestMenjacnice implements ValutaInterfejs {
 
+	private LinkedList<Valuta> valute = new LinkedList<>();
+
 	@Override
 	public void dodajKursValuteNaOdredjeniDan(String nazivValute, Kurs kurs, GregorianCalendar naDan) {
-		// TODO Auto-generated method stub
-
+		for (int i = 0; i < valute.size(); i++) {
+			Valuta valuta = valute.get(i);
+			if (valuta.getNaziv().equals(nazivValute)) {
+				LinkedList<Kurs> kursevi = valuta.getKursevi();
+				for (int j = 0; j < kursevi.size(); j++) {
+					if (kursevi.get(j).getNaDan().equals(naDan)) {
+						throw new RuntimeException("Kurs za dati dan vec postoji");
+					}
+				}
+				valuta.getKursevi().add(kurs);
+				return;
+			}
+		}
 	}
 
 	@Override
 	public void obrisiKursValuteNaOdredjeniDan(String nazivValute, Kurs kurs, GregorianCalendar naDan) {
-		// TODO Auto-generated method stub
-
+		for (int i = 0; i < valute.size(); i++) {
+			Valuta valuta = valute.get(i);
+			if (valuta.getNaziv().equals(nazivValute)) {
+				LinkedList<Kurs> kursevi = valuta.getKursevi();
+				for (int j = 0; j < kursevi.size(); j++) {
+					if (kursevi.get(j).getNaDan().equals(naDan)) {
+						valuta.getKursevi().remove(j);
+						return;
+					}
+				}
+			}
+		}
 	}
 
 	@Override
 	public Kurs vratiKursValuteNaOdredjeniDan(String nazivValute, GregorianCalendar naDan) {
-		// TODO Auto-generated method stub
-		return null;
+		for (int i = 0; i < valute.size(); i++) {
+			Valuta valuta = valute.get(i);
+			if (valuta.getNaziv().equals(nazivValute)) {
+				LinkedList<Kurs> kursevi = valuta.getKursevi();
+				for (int j = 0; j < kursevi.size(); j++) {
+					if (kursevi.get(j).getNaDan().equals(naDan)) {
+						return valuta.getKursevi().get(j);
+					}
+				}
+			}
+		}
+		throw new RuntimeException("Ne postoji kurs sa datim datumom");
 	}
 
 }
