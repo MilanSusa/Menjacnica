3c64afcd4cd881913311ce88905c17342915f5df
MilanSusa <milan_susa@hotmail.com>
2017-03-22 18:52:35 +0100

    Uvedena klasa Valuta u paketu valuta, uvedeni atributi naziv, skraceniNaziv i kurs i uvedene get i set metode za te atribute.

---
+++ Menjacnica/src/valuta/Valuta.java
@@ -0,0 +1,33 @@
+package valuta;
+
+public class Valuta {
+
+	private String naziv;
+	private String skraceniNaziv;
+	private String kurs;
+
+	public String getNaziv() {
+		return naziv;
+	}
+
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+
+	public String getSkraceniNaziv() {
+		return skraceniNaziv;
+	}
+
+	public void setSkraceniNaziv(String skraceniNaziv) {
+		this.skraceniNaziv = skraceniNaziv;
+	}
+
+	public String getKurs() {
+		return kurs;
+	}
+
+	public void setKurs(String kurs) {
+		this.kurs = kurs;
+	}
+
+}
